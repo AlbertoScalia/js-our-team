b33fe12af66b211452967d1196402c11716ec814
AlbertoScalia <albscalia@gmail.com>
2023-05-02 16:11:53 +0200

    Feat (Js & CSS) : Add function and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+//Assegno all'elemento .row una costante
+const contenitoreTeam = document.querySelector('.row');
+
 //Creo l'array con i membri del team
 const teamCompleto = [
     {
@@ -41,5 +44,23 @@ function arrayDaIterare(teamCompleto) {
     for (let i = 0; i < teamCompleto.length; i++) {
         //Assegno ad una variabile ogni oggetto dell'array
         const membroTeam = teamCompleto[i];
+        //Scrivo la funzione per far apparire le card
+        elementiDaInserire(contenitoreTeam, membroTeam);
     }
+}
+
+// Creo la funzione per far apparire le card quando si aprirà la pagina
+function elementiDaInserire(elementiDom, oggetto) {
+    const colonnaDaInserire = `
+    <div class="col-12 col-sm-6 col-lg-4">
+        <div class="card rounded-0 mb-4 shadow-sm ">
+            <img src="./assets/img/${oggetto.foto}" alt="${oggetto.nome}">
+            <div class="card-body text-center">
+                <h4>${oggetto.nome}</h4>
+                <p class="text-secondary">${oggetto.ruolo}</p>
+            </div>
+        </div>
+    </div>
+    `
+    elementiDom.innerHTML += colonnaDaInserire;
 }
